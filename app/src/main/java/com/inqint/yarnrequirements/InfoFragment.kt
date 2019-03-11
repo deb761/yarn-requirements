79918249b3a4311c2cfde7e9a3cb22a183c1e3fc
Deborah Engelmeyer <dengelmeyer@icloud.com>
2019-03-10 21:42:37 -0700

    Add icons8 link to info page

--- app/src/main/java/com/inqint/yarnrequirements/InfoFragment.kt
+++ app/src/main/java/com/inqint/yarnrequirements/InfoFragment.kt
@@ -1,12 +1,16 @@
 package com.inqint.yarnrequirements
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
+import android.widget.Button
 
-class InfoFragment : Fragment() {
+class InfoFragment : Fragment(), OnClickListener {
     companion object {
 
         fun newInstance(): InfoFragment {
@@ -15,6 +19,19 @@ class InfoFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-       return inflater.inflate(R.layout.fragment_info, container, false)
-   }
+        var mview = inflater.inflate(R.layout.fragment_info, container, false)
+        val button: Button = mview.findViewById(R.id.icon8Link)
+        button.setOnClickListener(this)
+        return mview
+    }
+
+    // Implement the OnClickListener callback
+    override fun onClick(v: View) {
+        // open the Icon8 web site
+        val webpage: Uri = Uri.parse("http://www.icons8.com")
+        val intent = Intent(Intent.ACTION_VIEW, webpage)
+        if (intent.resolveActivity(activity.packageManager) != null) {
+            startActivity(intent)
+        }
+    }
 }
